e8ec08237fa5256bd416d8bed7ef68d1f800a46d
adrian-nieto <Nieto.Adrian87@gmail.com>
2016-11-26 09:28:12 -0800

    added initial state, fixed tests

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,15 @@
-export default function reducer(state, action) {
-    //figure out which function to call and call it
+import {setEntries, next, vote, INITIAL_STATE} from './core';
+
+export default function reducer(state = INITIAL_STATE, action) {
+    switch (action.type) {
+        case 'SET_ENTRIES':
+            return setEntries(state, action.entries);
+        case 'NEXT':
+            return next(state);
+        case 'VOTE':
+            return vote(state, action.entry);
+            break;
+        default:
+            return state;
+    }
 }
--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -16,7 +16,7 @@ describe('reducer', () => {
     });
 
     it('handles NEXT', () => {
-        const initialState = fromjs({
+        const initialState = fromJS({
             entries: ['Trainspotting', '28 Days Later']
         });
         const action = {type: 'NEXT'};
@@ -48,5 +48,27 @@ describe('reducer', () => {
             entries: []
         }));
     });
-    
+
+    it('has an initial state', () => {
+        const action = {type: 'SET_ENTRIES', entries: ['Trainspotting']};
+        const nextState = reducer(undefined, action);
+        expect(nextState).to.equal(fromJS({
+            entries: ['Trainspotting']
+        }));
+    });
+
+    it('can be used with reduce', () => {
+        const actions = [
+            {type: 'SET_ENTRIES', entries: ['Trainspotting', '28 Days Later']},
+            {type: 'NEXT'},
+            {type: 'VOTE', entry: 'Trainspotting'},
+            {type: 'VOTE', entry: '28 Days Later'},
+            {type: 'VOTE', entry: 'Trainspotting'},
+            {type: 'NEXT'}
+        ];
+        const finalState = actions.reduce(reducer, Map());
+        expect(finalState).to.equal(fromJS({
+            winner: 'Trainspotting'
+        }));
+    });
 });
